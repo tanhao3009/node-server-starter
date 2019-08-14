f02b1b1361c3ffb962b5aa96347b6b8e3c8534a1
Hung Q. Truong <htq287@gmail.com>
2019-08-14 18:18:46 +0700

    * user signup - use Cookie-Based Auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import express = require("express");
+import compression from "compression";  // compresses requests
 import session from "express-session";
+import bodyParser from "body-parser";
 import bluebird from "bluebird";
+import lusca from "lusca";
+import passport from "passport";
+import flash from "express-flash";
 import mongoose from "mongoose";
 import mongo from "connect-mongo";
 import { MONGODB_URI, SESSION_SECRET } from "./utils/secrets";
 
 
+import * as userController from "./controllers/user";
 import * as homeController from "./controllers/home";
 
 const MongoStore = mongo(session);
@@ -35,6 +41,9 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true} ).then(
 
 // Express configuration
 app.set("port", process.env.PORT || 3001);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     resave: true,
     saveUninitialized: true,
@@ -45,10 +54,35 @@ app.use(session({
     })
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+    req.path !== "/login" &&
+    req.path !== "/signup" &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+    } else if (req.user &&
+    req.path == "/account") {
+        req.session.returnTo = req.path;
+    }
+    next();
+});
+
 /**
  * API examples routes.
  */
 
 app.get("/", homeController.getUsages);
+app.post("/signup", userController.postSignup);
 
 export default app;
---
+++ src/controllers/user.ts
@@ -0,0 +1,82 @@
+import async from "async";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+import passport from "passport";
+import { User, UserDocument, AuthToken } from "../models/User";
+import { Request, Response, NextFunction } from "express";
+import { IVerifyOptions } from "passport-local";
+import { WriteError } from "mongodb";
+import { check, sanitize, validationResult } from "express-validator";
+import "../config/config";
+
+/**
+ * POST /login
+ * Sign in using email and password.
+ */
+export const postLogin = (req: Request, res: Response, next: NextFunction) => {
+    check("email", "Email is not valid").isEmail();
+    check("password", "Password cannot be blank").isLength({min: 1});
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    //sanitize("email").normalizeEmail({ gmail_remove_dots: false });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        req.flash("errors", errors.array());
+        return res.redirect("/login");
+    }
+
+    passport.authenticate("local", (err: Error, user: UserDocument, info: IVerifyOptions) => {
+        if (err) { return next(err); }
+        if (!user) {
+            req.flash("errors", {msg: info.message});
+            return res.redirect("/login");
+        }
+        req.logIn(user, (err) => {
+            if (err) { return next(err); }
+            req.flash("success", { msg: "Success! You are logged in." });
+            res.redirect(req.session.returnTo || "/");
+        });
+    })(req, res, next);
+};
+
+/**
+ * POST /signup
+ * Create a new local account.
+ */
+export const postSignup = (req: Request, res: Response, next: NextFunction) => {
+    check("email", "Email is not valid").isEmail();
+    check("password", "Password must be at least 4 characters long").isLength({ min: 4 });
+    check("confirmPassword", "Passwords do not match").equals(req.body.password);
+    
+    //sanitize("email").normalizeEmail({ gmail_remove_dots: false });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        req.flash("errors", errors.array());
+        return res.redirect("/signup");
+    }
+
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    User.findOne({ email: req.body.email }, (err, existingUser) => {
+        if (err) { return next(err); }
+        if (existingUser) {
+            req.flash("errors", { msg: "Account with that email address already exists." });
+            return res.redirect("/signup");
+        }
+        user.save((err) => {
+            if (err) { return next(err); }
+            req.logIn(user, (err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect("/");
+            });
+        });
+    });
+};
